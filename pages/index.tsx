c878f0a35e5844e9683109b88ce5d08b88b4fdc0
Mogasala Hemagiri <hemagiri1394.in@gmail.com>
2025-03-23 17:18:47 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import Image from "next/Image";
+import Image from "next/image";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
